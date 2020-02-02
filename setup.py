09904a6fa0581885072e070298ae6e6cf802c403
hanxinzhu <xinzhuhan@wustl.edu>
2020-02-02 14:56:39 -0600

    .

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='jsut demo',
+    author='Xinzhu',
+    license='MIT',
+)
